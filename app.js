cca5c9c40c3a67cab0088b86c4db557796dbc66b
rameezqazi23 <rameez.qazi23@gmail.com>
2023-09-01 00:34:55 +0500

    creating environment variable for deploying application on elastic beanstalk uploading version 1.0.0

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const path = require("path");
 const cookieParser = require("cookie-parser");
 const { connectToMongoDb } = require("./connect");
 const { checkAuthenticationCookie } = require("./middlewares/auth");
+require('dotenv').config()
 
 const userRoute = require("./routes/user");
 const blogRoute = require("./routes/blog");
@@ -10,7 +11,10 @@ const Blog = require("./models/blog");
 
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || PORT;
+connectToMongoDb(process.env.MONGO_URL)
+    .then(() => console.log("MongoDb successfully connected!"))
+    .catch((err) => console.log("MongoDb connection error ", err))
 
 //Middleware
 app.use(express.json());
@@ -38,12 +42,6 @@ app.get("/", async (req, res) => {
 //Routes---
 app.use("/user", userRoute)
 app.use("/blog", blogRoute)
-
-
-
-connectToMongoDb("mongodb://127.0.0.1:27017/blog-app")
-    .then(() => console.log("MongoDb successfully connected!"))
-    .catch((err) => console.log("MongoDb connection error ", err))
 
 
 app.listen(PORT, () => console.log("Server running on PORT: ", PORT))
